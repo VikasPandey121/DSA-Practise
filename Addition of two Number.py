ddcd2ee305a3be3683f5a72fe3ac8772b39a491e
Vikas Kumar <12.vikaskumar99@gmail.com>
2019-11-25 01:43:43 +0530

    Added 1st program

---
+++ Addition of two Number.py
@@ -0,0 +1,8 @@
+numberOne = input('Enter the Value ')
+numberTwo = input('Enter the value')
+numberOne = int(numberOne)
+numberTwo = int(numberTwo)
+
+print('calculating Result')
+result = numberOne + numberTwo
+print ('The result is: ' + str(result))
